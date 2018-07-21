b59ff5f433adfab405211fa0fbad5c8ff67cf31e
594zyc <502744128@qq.com>
2018-07-22 00:40:00 +0800

    国际港澳台和家庭多终端

--- data/DataBase/ExcelToSqliteDB.py
+++ data/DataBase/ExcelToSqliteDB.py
@@ -3,9 +3,10 @@
 生成过程存在很多人为干预
 """
 import xlrd, re, sqlite3,copy, pprint
-from data.DataBase.Ontology import TaoCan_DB_slots, \
+from OntologyConstruction import TaoCan_DB_slots, \
     LiuLiang_DB_slots,WLAN_DB_slots,Card_DB_slots
-
+from OntologyConstruction import MultiTerminal_DB_slots
+from OntologyConstruction import Overseas_DB_slots
 
 def excel_reader_TAOCAN():
     data = xlrd.open_workbook('NewOntology.xlsx')
@@ -175,7 +176,7 @@ def extract_TAOCAN():
     cur.execute(exec_create_command)
 
     for data in excel_reader_TAOCAN():
-        print(data)
+        # print(data)
         if data["功能费"]:
             m = re.match(r"(\d+).*", data["功能费"])
             data["功能费"] = int(m.group(1))
@@ -191,7 +192,7 @@ def extract_TAOCAN():
 
 
 
-        exec_insert_command = "INSERT INTO 套餐 (" +\
+        exec_insert_command = "INSERT INTO 套餐(" +\
             ",".join([slot for slot in TaoCan_DB_slots]) +")\n" +\
             "VALUES ("
         for slot in TaoCan_DB_slots:
@@ -759,9 +760,333 @@ def extract_Card():
     conn.close()
 
 
+def excel_reader_MultiTerminal():
+    data = xlrd.open_workbook('NewOntology.xlsx')
+    table = data.sheet_by_name('家庭及多终端')  # 通过名称获取
+    nrows = table.nrows
+    DataBaseList = []
+
+    Default_DataItemValues = {
+        "子业务":None,
+        "主业务":None,
+        "产品介绍":None,
+        "计费方式": None,
+        "适用品牌": "全球通、动感地带、神州行",
+        "功能费": None,
+        "副卡数量上限": None,
+        "套餐内容_通话共享规则":None,
+        "套餐内容_短信共享规则":None,
+        "套餐内容_流量共享规则":None,
+        "套餐内容_其他功能":None,
+        "结转规则_国内流量": "当月未消费的剩余流量（不含促销赠送的套外流量及定向视频流量），可以结转至次月使用，结转流量次月月底失效。同一类型的流量 优先扣减上月结转的剩余流量，再扣减套餐内流量。不同类型的流量，按照原有的扣费顺序扣减，计费规则不变。若办理了套餐变更（含档位变更、变更回原套餐）、手机处于停机销号则流量无法结转",
+        "是否包含港澳台地区": "不包含港澳台",
+        "能否结转滚存": "能",
+        "能否分享": "能",
+        "能否转赠": "能",
+        "流量分享方式": "登录移动APP客户端后，选择办理，点击“流量共享”，按提示操作即可",
+        "流量转赠方式" : "登录移动官方APP，点击“剩余流量”界面的“流量转赠”，按提示操作即可",
+        "转户转品牌管理": None,
+        "停机销号管理": "若您整月处于停机状态（含申停、欠停），不收取当月功能费或移动数据流量套餐费，已购服务有效期不延期，剩余流量无法结转，不支持退换费用。",
+        "赠送优惠活动": None,
+        "使用有效期":None,
+        "封顶规则": None,
+        "限速说明": None,
+        "受理时间": "每月最后一天19点后无法办理",
+        "主卡互斥业务": None,
+        "副卡互斥业务": None,
+        "主卡开通客户限制": None,
+        "副卡客户限制": None,
+        "主卡套餐限制": None,
+        "其他开通限制": None,
+        "开通方式": "通过营业厅、10086人工、微厅、北京移动手机营业厅等方式开通",
+        "取消方式": "通过营业厅、10086人工、微厅、北京移动手机营业厅等方式取消",
+        "主卡添加成员": None,
+        "主卡删除成员": None,
+        "副卡成员主动退出": None,
+        "主卡查询副卡": None,
+        "副卡查询主卡": None,
+        "恢复流量功能": None,
+        "开通生效规则": "新入网客户或未开通主套餐的客户当月开通，当月生效。当月的月功能费及免费资源按天折算。已办理其他套餐的客户，申请开通套餐时，直接开通新套餐即可，无需对原套餐进行取消，当月办理下月生效。",
+        "取消变更生效规则": "当月变更/取消，下月生效",
+        "能否变更或取消": "该套餐可以变更或取消",
+        "是否到期自动取消": "不是",
+        "是否自动顺延到下月": "是"
+    }
+    slot_excel_column_id={
+        "子业务":0,
+        "主业务":1,
+        "产品介绍": 2,
+        "计费方式": 3,
+        "适用品牌": 4,
+        "功能费": 5,
+        "副卡数量上限": 6,
+        "套餐内容_通话共享规则":7,
+        "套餐内容_短信共享规则":8,
+        "套餐内容_流量共享规则":9,
+        "套餐内容_其他功能":10,
+        "结转规则_国内流量": 11,
+        "是否包含港澳台地区": 12,
+        "能否结转滚存":13,
+        "能否分享": 14,
+        "能否转赠": 16,
+        "流量分享方式": 15,
+        "流量转赠方式" : 17,
+        "转户转品牌管理": 18,
+        "停机销号管理": 19,
+        "赠送优惠活动": 20,
+        "使用有效期":22,
+        "封顶规则": 24,
+        "限速说明": 25,
+        "受理时间": 26,
+        "主卡互斥业务": 27,
+        "副卡互斥业务": 28,
+        "主卡开通客户限制": 29,
+        "副卡客户限制": 30,
+        "主卡套餐限制": 31,
+        "其他开通限制": 32,
+        "开通方式": 33,
+        "取消方式": 34,
+        "主卡添加成员": 35,
+        "主卡删除成员": 36,
+        "副卡成员主动退出": 37,
+        "主卡查询副卡": 38,
+        "副卡查询主卡": 39,
+        "恢复流量功能": 40,
+        "开通生效规则": 41,
+        "取消变更生效规则": 42,
+        "是否自动顺延到下月": 43,
+        "是否到期自动取消": 44,
+        "能否变更或取消": 45,
+    }
+
+    for i in range(1, nrows):
+        DataItem = {}
+        table_values = table.row_values(i)
+        if table_values[0] == "" or table_values[0] == " ":
+            PrimaryItem = table_values
+
+        for slot in Default_DataItemValues:
+            if table_values[slot_excel_column_id[slot]] != ""and \
+            table_values[slot_excel_column_id[slot]] != " ":
+                DataItem[slot] = table_values[slot_excel_column_id[slot]]
+            elif PrimaryItem[slot_excel_column_id[slot]] != ""and \
+                    PrimaryItem[slot_excel_column_id[slot]] != " ":
+                DataItem[slot] = PrimaryItem[slot_excel_column_id[slot]]
+            else:
+                DataItem[slot] = Default_DataItemValues[slot]
+        temp_str = ""
+        for s in ["套餐内容_通话共享规则", "套餐内容_短信共享规则",
+                  "套餐内容_流量共享规则", "套餐内容_其他功能"]:
+            if DataItem[s] !=None:
+                temp_str += "%s：%s \n" %(s[5:], DataItem[s])
+        DataItem["套餐内容"] = temp_str
+
+        if DataItem["子业务"] != None:
+            DataItem["主业务"] = None
+        DataBaseList.append(copy.deepcopy(DataItem))
+
+        # print(table.row_values(i))
+        # DataItem = {'子业务':}
+    return DataBaseList
+
+
+def extract_MultiTerminal():
+    """
+    读取家庭多终端业务，并存为数据库
+    :return: 返回知识表 dict list
+    """
+    conn = sqlite3.connect("CMCC_NewDB.db")
+    cur = conn.cursor()
+    # create table
+    exec_create_command = "CREATE TABLE 家庭多终端("
+    for slot in MultiTerminal_DB_slots:
+        exec_create_command += "%s TEXT ," % slot
+    exec_create_command = exec_create_command[:-1]
+    exec_create_command += ");"
+    cur.execute(exec_create_command)
+
+    for data in excel_reader_MultiTerminal():
+        print(data)
+        exec_insert_command = "INSERT INTO 家庭多终端 (" +\
+            ",".join([slot for slot in MultiTerminal_DB_slots]) +")\n" +\
+            "VALUES ("
+        for slot in MultiTerminal_DB_slots:
+            if data[slot] == None:
+                exec_insert_command += 'NULL,'
+            else:
+                exec_insert_command += '\''+data[slot]+'\','
+        exec_insert_command = exec_insert_command[:-1]
+        exec_insert_command += ')'
+        print(exec_insert_command)
+        cur.execute(exec_insert_command)
+
+    # close DB
+    conn.commit()
+    conn.close()
+
+
+def excel_reader_Overseas():
+    data = xlrd.open_workbook('NewOntology.xlsx')
+    table = data.sheet_by_name('国际港澳台')  # 通过名称获取
+    nrows = table.nrows
+    DataBaseList = []
+
+    Default_DataItemValues = {
+        "子业务":None,
+        "主业务":None,
+        "产品介绍":None,
+        "开通方向": None,
+        "开通天数": None,
+        "计费方式": None,
+        "功能费": None,
+        "套餐内容_通话时长": None,
+        "套餐内容_短信": None,
+        "套餐内容_彩信": None,
+        "套餐内容_数据流量":None,
+        "套餐内容_其他功能":None,
+        "超出处理_通话时长": None,
+        "超出处理_短信": None,
+        "超出处理_数据流量":None,
+        "能否结转滚存": "不能",
+        "能否分享": "不能",
+        "能否转赠": "不能",
+        "流量分享方式": "登录移动APP客户端后，选择办理，点击“流量共享”，按提示操作即可",
+        "流量转赠方式" : "登录移动官方APP，点击“剩余流量”界面的“流量转赠”，按提示操作即可",
+        "转户转品牌管理": None,
+        "停机销号管理": "若您整月处于停机状态（含申停、欠停），不收取当月功能费或移动数据流量套餐费，已购服务有效期不延期，剩余流量无法结转，不支持退换费用。",
+        "赠送优惠活动": None,
+        "使用限制": None,
+        "封顶规则": None,
+        "限速说明": None,
+        "受理时间": "每月最后一天19点后无法办理",
+        "互斥业务": None,
+        "开通客户限制": None,
+        "累次叠加规则": None,
+        "开通方式": "通过营业厅、10086人工、微厅、北京移动手机营业厅等方式开通",
+        "开通生效规则": "新入网客户或未开通主套餐的客户当月开通，当月生效。当月的月功能费及免费资源按天折算。已办理其他套餐的客户，申请开通套餐时，直接开通新套餐即可，无需对原套餐进行取消，当月办理下月生效。",
+        "是否到期自动取消": "是",
+        "能否变更或取消": "该套餐可以变更或取消",
+        "取消方式": "通过营业厅、10086人工、微厅、北京移动手机营业厅等方式取消",
+        "到期失效规则": "到期自动失效"
+    }
+    slot_excel_column_id={
+        "子业务":0,
+        "主业务":1,
+        "产品介绍": 2,
+        "开通方向": 3,
+        "开通天数": 4,
+        "计费方式": 5,
+        "功能费": 6,
+        "套餐内容_通话时长": 7,
+        "套餐内容_短信": 8,
+        "套餐内容_彩信": 9,
+        "套餐内容_数据流量":10,
+        "套餐内容_其他功能":11,
+        "超出处理_通话时长": 12,
+        "超出处理_短信": 13,
+        "超出处理_数据流量":14,
+        "能否结转滚存": 15,
+        "能否分享": 16,
+        "能否转赠": 18,
+        "流量分享方式": 17,
+        "流量转赠方式" : 19,
+        "转户转品牌管理": 20,
+        "停机销号管理": 21,
+        "赠送优惠活动": 22,
+        "使用限制": 23,
+        "封顶规则": 24,
+        "限速说明": 25,
+        "受理时间": 26,
+        "互斥业务": 27,
+        "开通客户限制": 28,
+        "累次叠加规则": 29,
+        "开通方式": 30,
+        "开通生效规则": 31,
+        "是否到期自动取消": 32,
+        "能否变更或取消": 33,
+        "取消方式": 34,
+        "到期失效规则":35
+    }
+
+    for i in range(1, nrows):
+        DataItem = {}
+        table_values = table.row_values(i)
+        if table_values[0] == "" or table_values[0] == " ":
+            PrimaryItem = table_values
+
+        for slot in Default_DataItemValues:
+            if table_values[slot_excel_column_id[slot]] != ""and \
+            table_values[slot_excel_column_id[slot]] != " ":
+                DataItem[slot] = table_values[slot_excel_column_id[slot]]
+            elif PrimaryItem[slot_excel_column_id[slot]] != ""and \
+                    PrimaryItem[slot_excel_column_id[slot]] != " ":
+                DataItem[slot] = PrimaryItem[slot_excel_column_id[slot]]
+            else:
+                DataItem[slot] = Default_DataItemValues[slot]
+        DataItem["套餐内容"] = DataItem["产品介绍"]
+        DataItem["超出处理"] = DataItem["超出处理_通话时长"]
+        if DataItem["子业务"] != None:
+            DataItem["主业务"] = None
+        DataBaseList.append(copy.deepcopy(DataItem))
+
+        # print(table.row_values(i))
+        # DataItem = {'子业务':}
+    return DataBaseList
+
+
+def extract_Overseas():
+    """
+    读取国际港澳台业务，并存为数据库
+    :return: 返回知识表 dict list
+    """
+    conn = sqlite3.connect("CMCC_NewDB.db")
+    cur = conn.cursor()
+    # create table
+    exec_create_command = "CREATE TABLE 国际港澳台("
+    for slot in Overseas_DB_slots:
+        if slot in ["开通天数"]:
+            exec_create_command += "%s NUMERIC," % slot
+        else:
+            exec_create_command += "%s TEXT ," % slot
+    exec_create_command = exec_create_command[:-1]
+    exec_create_command += ");"
+    cur.execute(exec_create_command)
+
+    for data in excel_reader_Overseas():
+        print(data)
+        if data["开通天数"]:
+            m = re.match(r"(\d+).*", data["开通天数"])
+            print(m)
+            if m is not None:
+                data["开通天数"] = int(m.group(1))
+        exec_insert_command = "INSERT INTO 国际港澳台 (" +\
+            ",".join([slot for slot in Overseas_DB_slots]) +")\n" +\
+            "VALUES ("
+        for slot in Overseas_DB_slots:
+            if slot in ["开通天数"]:
+                if data[slot]  == None:
+                    exec_insert_command += 'NULL,'
+                else:
+                    exec_insert_command += str(data[slot])+','
+            else:
+                if data[slot] == None:
+                    exec_insert_command += 'NULL,'
+                else:
+                    exec_insert_command += '\''+data[slot]+'\','
+        exec_insert_command = exec_insert_command[:-1]
+        exec_insert_command += ')'
+        print(exec_insert_command)
+        cur.execute(exec_insert_command)
+
+    # close DB
+    conn.commit()
+    conn.close()
+
 
 if __name__ == '__main__':
-    extract_TAOCAN()
-    extract_LIULIANG()
-    extract_WLAN()
-    extract_Card()
+    # extract_TAOCAN()
+    # extract_LIULIANG()
+    # extract_WLAN()
+    # extract_Card()
+    # extract_MultiTerminal()
+    extract_Overseas()
--- data/DataBase/Operation.py
+++ data/DataBase/Operation.py
@@ -17,17 +17,17 @@ class DBOperationWrapper:
         文字描述的slots 统一 按“高”“中”“低”搜索
         :param table: 领域表名
         :param feed_dict: 搜索条件
-        :return: 搜索结果,按价格排序
+        :return: 搜索结果
         """
         if table == "国际港澳台":
             command = """
                 SELECT * FROM 国际港澳台
-                WHERE  
+                WHERE
                 """
             constraints = []
             if "开通天数" in feed_dict:
-                constraints.append("开通天数 == %d" % feed_dict["开通天数"])
-            for slot in ["开通国家", "开通地区"]:
+                constraints.append("开通天数 == %d" % (feed_dict["开通天数"]))
+            for slot in ["开通国家"]:  #“开通地区”暂未考虑
                 if slot in feed_dict:
                     constraints.append("{0} LIKE \'%{1}%\'".format(slot,feed_dict[slot]))
             command += " AND ".join(constraints)
@@ -35,7 +35,7 @@ class DBOperationWrapper:
         elif table == "套餐":
             command = """
                             SELECT * FROM 套餐
-                            WHERE  
+                            WHERE
                             """
             constraints = []
             for slot in ["功能费", "套餐内容_国内流量", "套餐内容_国内主叫"]:
@@ -64,12 +64,12 @@ class DBOperationWrapper:
                 else:
                     constraints.append("套餐内容_国内主叫 <= 400.0")
 
-            command += " AND ".join(constraints) + "\nORDER BY 套餐内容_国内流量 DESC"
+            command += " AND ".join(constraints)
             return self.cur.execute(command)
         elif table == "流量":
             command = """
                             SELECT * FROM 流量
-                            WHERE  
+                            WHERE
                             """
             constraints = []
             for slot in ["功能费", "套餐内容_国内流量"]:
@@ -90,7 +90,7 @@ class DBOperationWrapper:
                 else:
                     constraints.append("套餐内容_国内流量 <= 400.0")
 
-            command += " AND ".join(constraints) + "\nORDER BY 套餐内容_国内流量 DESC"
+            command += " AND ".join(constraints)
             return self.cur.execute(command)
         else:
             return None
@@ -99,7 +99,7 @@ class DBOperationWrapper:
         if table == "Card":
             command = """
                     SELECT * FROM %s
-                    WHERE  
+                    WHERE
                     """%(table)
             constraints = []
             for slot in ["号卡"]:
@@ -125,8 +125,6 @@ class DBOperationWrapper:
 
 if __name__ == '__main__':
     operation = DBOperationWrapper('../tmp/CMCC_NewDB.db')
-    # for ii in operation.SearchingByConstraints("流量", {"套餐内容_国内流量_文字描述": "高"}):
-    #     print(dict(zip(LiuLiang_DB_slots, ii)))
-    for ii in operation.SearchingByEntity("流量", {"子业务": '20元地铁流量包'}):
+    for ii in operation.SearchingByConstraints("流量", {"套餐内容_国内流量_文字描述": "高"}):
         print(dict(zip(LiuLiang_DB_slots, ii)))
 
