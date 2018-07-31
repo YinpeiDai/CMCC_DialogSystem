6507e7ff821ba0ab306dd42e7e062939427f34e9
zhangyc <502744128@qq.com>
2018-08-01 00:36:27 +0800

    basic code of DST updating

--- DM/DST/StateTracking.py
+++ DM/DST/StateTracking.py
@@ -7,24 +7,81 @@
 是否 DST 不变、是否用户重复表述同一句话、情感检测结果
 
 """
-from collections import deque
+import sys
+sys.path.append('../..')
+# from collections import deque
+from NLU.NLUManager import *
 
-class DialodStateTracker:
-    def __init__(self):
-        self.MentionedEntitySet = deque([], maxlen=2)
-        self.PersonalEntitySet = None
-        self.SearchedEntitySet = None
-        self.ReturnedEntitySet = None
-        self.requested_slots = deque([], maxlen=2)
-        self.detected_domain = deque([], maxlen=2)
-        self.user_acts = deque([], maxlen=2)
+class BeliefState():
+    pass
+DialogStateSample = {
+    'TurnNum':int,
+    # 用户主动提及业务实体集合
+    'EntityMentioned': {'curr_turn': [],
+                        'prev_turn': []},
+    'UserPersonal': {
+            "已购业务": ["180元档幸福流量年包", "18元4G飞享套餐升级版"], # 这里应该是完整的业务的信息dict
+            "使用情况": "剩余流量 11.10 GB，剩余通话 0 分钟，话费余额 110.20 元，本月已产生话费 247.29 元",
+            "号码": "18811369685",
+            "归属地" : "北京",
+            "品牌": "动感地带",
+            "是否转品牌过渡期": "否",
+            "话费查询": "话费余额 110.20 元",
+            "流量查询": "剩余流量 11.10 GB",
+            "订购时间": "订购时间 2017-04-04， 生效时间 2017-05-01",
+            "是否停机": "否",
+            "话费充值": "请登录网上营业厅、微厅或 APP 充值",
+            "流量充值": "请登录网上营业厅、微厅或 APP 充值",
+            "账单查询": "请登录网上营业厅、微厅或 APP 查询"
+        }, # 个人信息，dict
+    'QueryResults': [], # 查询数据库得到的list of 业务信息dict
+    # 一轮就提供一个套餐
+    'OfferedResult': {'prev_turn':{'子业务': '188元畅享套餐', '主业务': None, '功能费': 188, '套餐内容_国内主叫': 700, '套餐内容_国内流量': 12288},
+                    'preprev_turn': {'子业务': '188元畅享套餐', '主业务': None, '功能费': 188, '套餐内容_国内主叫': 700, '套餐内容_国内流量': 12288}},
+    'SystemAct': {'curr_turn': {'offer': {'子业务': '158元畅享套餐', '主业务': None, '功能费': 158, '套餐内容_国内主叫': 700, '套餐内容_国内流量': 6144},
+                                      'inform':['适用品牌', '产品介绍']},
+                     'prev_turn': {}},
+    'BeliefState': {'curr_turn':{"功能费": [150, 300]},
+                    'prev_turn':{"功能费": [150, 300]}},
+    'RequestedSlot': {'curr_turn': ["套餐内容_国内流量", "副卡客户限制"],
+                      'prev_turn': []},
+    'DetectedDomain': {'curr_turn': "套餐",
+                         'prev_turn': None},
+    'UserAct': {'curr_turn': "告知",
+                'prev_turn': None}
+}
+
+
+class DialogStateTracker:
+    def __init__(self, usr_personal):
+        # dialog state objects
+        self.EntityMentioned =  {'curr_turn': None,  'prev_turn': None}
+        self.UserPersenal = usr_personal
+        self.QueryResult = None
+        self.SystemReturnAct = None
+        self.BeliefState = {'curr_turn': None,  'prev_turn': None}
+        self.RequestedSlot = {'curr_turn': None,  'prev_turn': None}
+        self.DetectedDomain = {'curr_turn': None,  'prev_turn': None}
+        self.UserAct = {'curr_turn': None,  'prev_turn': None}
+        self.TurnNum = 0
+        self.DialogState = {
+            'TurnNum': self.TurnNum,
+            'EntityMentioned': self.EntityMentioned,
+            'UserPersenal': self.UserPersenal,
+            'QueryResult': self.QueryResult,
+            'SystemReturnAct': self.SystemReturnAct,
+            'BeliefState': self.BeliefState,
+            'RequestedSlot': self.RequestedSlot,
+            'DetectedDomain':self.DetectedDomain,
+            'UserAct': self.UserAct
+        }
+        # status variables
         self.isDSTChange = True
         self.isUtterSame = False
         self.detected_sentiment = None
         self.user_utter = None
-        self.belief_states = {}
 
-    def update(self, NLU_results, data_manager):
+    def update(self, NLU_results, rule_policy, data_manager):
         """
         NLU_results 包含
         Dom_results, ER_results, Senti_results, UserAct_results,
@@ -32,18 +89,26 @@ class DialodStateTracker:
         :param NLU_results: dict
         :param data_manager: data_manager
         """
+        self.TurnNum += 1
+        # 复制上一轮的dialog state到prev_turn
+        for key,value in self.DialogState.items():
+            if 'prev_turn' in value.keys():
+                value['prev_turn'] = value['curr_turn']
+
+        # 利用本轮NLU结果更新当前轮的dialog state
+        self.EntityMentioned['curr_turn'] = NLU_results['entity']
+        self.BeliefState['curr_turn'] = NLU_results['informable']
+        self.RequestedSlot['curr_turn'] = NLU_results['requestable']
+        self.DetectedDomain['curr_turn'] =NLU_results['domain']
+        self.UserAct['curr_turn'] = NLU_results['useract']
+        # TODO: sentiment
+
+        self.UserPersenal = self.UserPersenal  #个人信息不变
+        self.QueryResult = DataManager.SearchingByConstraints(
+            table=NLU_results['domain'], feed_dict=NLU_results['informable'])
+        self.SystemReturnAct = rule_policy.Reply(self.DialogState)
+
         pass
-
-
-
-
-
-
-
-
-
-
-
 
 
 
