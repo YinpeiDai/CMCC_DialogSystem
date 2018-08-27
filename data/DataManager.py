4abafae2d28dd204583ec4d37d1b4a16581cd3b5
YIN <1@1.com>
2018-08-27 21:04:41 +0100

    fix 数据库 bug

--- data/DataManager.py
+++ data/DataManager.py
@@ -297,7 +297,7 @@ if __name__ == '__main__':
 
     # 测试 DataManager
     data_manager = DataManager('./tmp')
-    for ii in data_manager.SearchingByConstraints('套餐', {"功能费": [0,50]}):
+    for ii in data_manager.SearchingByConstraints('流量', {"套餐内容_国内流量_文字描述": 低}):
         print(ii['子业务'])
     # for ii in data_manager.SearchingByEntity("套餐", {"子业务": '88元畅享套餐'}):
     #     print(ii)
